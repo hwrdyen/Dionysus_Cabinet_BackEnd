6fefd3220b783ccd6a72eb0a2769aab00ba43693
Howard Yen <hwrdpvt@gmail.com>
2023-02-14 21:53:32 -0500

    First draft of backend

--- routes/Dionysus_Cabinet_Routes.js
+++ routes/Dionysus_Cabinet_Routes.js
@@ -7,16 +7,21 @@ router.use((req, res, next) => {
     next();
 });
 
+// GET command
+
+// Get /
 router.get("/", (req, res) => {
     res.send("Express Homepage");
 });
 
+// Get /cocktails_list
 router.get("/cocktails_list", (req, res) => {
     let cocktail_recipes_data = fs.readFileSync("data/cocktail_recipes.json");
     let parse_cocktail_recipes_data = JSON.parse(cocktail_recipes_data);
     res.json(parse_cocktail_recipes_data);
 });
 
+// Get /cocktails_list/:cocktailId
 router.get("/cocktails_list/:cocktailId", (req, res) => {
     const cocktailId = req.params.cocktailId;
     let cocktail_recipes_data = fs.readFileSync("data/cocktail_recipes.json");
@@ -25,6 +30,30 @@ router.get("/cocktails_list/:cocktailId", (req, res) => {
     res.json(currentCocktailDetails);
 });
 
+// Get /taste_preferences_list
+router.get("/taste_preferences_list", (req, res) => {
+    let taste_preferences_list_data = fs.readFileSync("data/taste_preferences_list.json");
+    let parse_taste_preferences_list_data = JSON.parse(taste_preferences_list_data);
+    res.json(parse_taste_preferences_list_data);
+});
+
+// Get /alcohol_list
+router.get("/alcohol_list", (req, res) => {
+    let alcohol_list_data = fs.readFileSync("data/alcohol_list.json");
+    let parse_alcohol_list_data = JSON.parse(alcohol_list_data);
+    res.json(parse_alcohol_list_data);
+});
+
+// Get /represent_mood_list
+router.get("/represent_mood_list", (req, res) => {
+    let represent_mood_list_data = fs.readFileSync("data/represent_mood_list.json");
+    let parse_represent_mood_list_data = JSON.parse(represent_mood_list_data);
+    res.json(parse_represent_mood_list_data);
+});
+
+// POST command
+
+// Post /cocktails_list
 router.post("/cocktails_list", (req, res) => {
     let new_data = (req.body);
     let cocktail_recipes_data = fs.readFileSync("data/cocktail_recipes.json");
